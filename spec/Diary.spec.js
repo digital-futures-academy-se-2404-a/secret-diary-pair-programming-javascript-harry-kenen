3d3480bac643cd9fea76db15c104986d6392cb2e
Kenen <kenen1996@hotmail.co.uk>
2024-04-23 15:23:56 +0100

    first test

---
+++ spec/Diary.spec.js
@@ -0,0 +1,16 @@
+import Diary from '../src/Diary';
+
+describe('Diary entry tests:', () => {
+
+  beforeEach(() => {
+    const testDiary = new Diary();
+  })
+
+  it("should store text inputs provided by the user", () => {
+    const testEntry = "Hello, World!";
+
+    testDiary.addEntry(testEntry);
+
+    expect(testDiary.entries.length).toBe(1);
+  })
+})
